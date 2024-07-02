5e7b18b503e450c896fd0994e2b0949702c36b8f
Deep  Panday <106895885+DeepPanday047@users.noreply.github.com>
2024-07-02 14:07:36 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,8 +5,8 @@ def fetch_cricket_scores():
     url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent"
 
     headers = {
-        "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-        "X-RapidAPI-Key": "Replace with your RapidAPI key"  # Replace with your RapidAPI key
+        'x-rapidapi-key': '8cf6521a06msh2e1499b6f52c2b6p19f978jsn71b595fbe7fa',
+		'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com'
     }
     response = requests.get(url, headers=headers)
     data = response.json()
